c258850530951b3a3c0136a27e8a5b320cfac906
Cypo <voodoo.7@outlook.com>
2019-12-16 17:16:56 +0800

    init

---
+++ src/main/java/cn/edu/bjtu/ebosnotice/EbosNoticeApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.bjtu.ebosnotice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbosNoticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EbosNoticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/edu/bjtu/ebosnotice/EbosNoticeApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.bjtu.ebosnotice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbosNoticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
