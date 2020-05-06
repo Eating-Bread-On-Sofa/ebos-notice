119622eead1272f5a664bfd16775bd68bf9cac96
Cypo <voodoo.7@outlook.com>
2020-05-06 18:17:12 +0800

    add swagger & query

--- src/main/java/cn/edu/bjtu/ebosnotice/controller/NoticeController.java
+++ src/main/java/cn/edu/bjtu/ebosnotice/controller/NoticeController.java
@@ -4,11 +4,16 @@ import cn.edu.bjtu.ebosnotice.dao.NoticeRepo;
 import cn.edu.bjtu.ebosnotice.entity.Notice;
 import com.alibaba.fastjson.JSONArray;
 import cn.edu.bjtu.ebosnotice.service.LogService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
+@Api(tags = "通知管理")
 @RequestMapping("/api/notice")
 @RestController
 public class NoticeController {
@@ -17,12 +22,32 @@ public class NoticeController {
     @Autowired
     LogService logService;
 
+    @ApiImplicitParam(name = "days",value = "查询天数范围,int类型",required = true, paramType = "query")
     @CrossOrigin
     @GetMapping("/alert")
-    public JSONArray getByType(){
-        List<Notice> notices = noticeRepo.findByType("alert");
+    public JSONArray getByType(@RequestParam int days){
+        Date time = daysAgo(days);
+        List<Notice> notices = noticeRepo.findByTypeIsAndCreatedAfter("alert",time);
         JSONArray jsonArray = new JSONArray();
         jsonArray.addAll(notices);
         return jsonArray;
+    }
+
+    @ApiImplicitParam(name = "days",value = "查询天数范围,int类型",required = true, paramType = "query")
+    @CrossOrigin
+    @GetMapping("/source/{source}")
+    public JSONArray getBySource(@PathVariable String source,@RequestParam int days){
+        Date time = daysAgo(days);
+        List<Notice> notices = noticeRepo.findBySourceIsAndCreatedAfter(source, time);
+        JSONArray jsonArray = new JSONArray();
+        jsonArray.addAll(notices);
+        return jsonArray;
+    }
+
+    private Date daysAgo(int days){
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(new Date());
+        calendar.add(Calendar.DATE, -days);
+        return calendar.getTime();
     }
 }
--- src/main/java/cn/edu/bjtu/ebosnotice/dao/NoticeRepo.java
+++ src/main/java/cn/edu/bjtu/ebosnotice/dao/NoticeRepo.java
@@ -11,9 +11,13 @@ import java.util.List;
 public interface NoticeRepo extends MongoRepository<Notice, String> {
     List<Notice> findByType(String type);
     List<Notice> findBySource(String source);
+    List<Notice> findByCreatedAfter(Date start);
     List<Notice> findByCreatedBetween(Date start, Date end);
     List<Notice> findByTypeAndSource(String type, String source);
+    List<Notice> findByTypeIsAndCreatedAfter(String type, Date start);
     List<Notice> findByTypeIsAndCreatedBetween(String type, Date start, Date end);
+    List<Notice> findBySourceIsAndCreatedAfter(String source, Date start);
     List<Notice> findBySourceIsAndCreatedBetween(String source, Date start, Date end);
+    List<Notice> findByTypeIsAndSourceIsAndCreatedAfter(String type, String source, Date start);
     List<Notice> findByTypeIsAndSourceIsAndCreatedBetween(String type, String source, Date start, Date end);
 }
